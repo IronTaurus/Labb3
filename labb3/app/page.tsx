22040a0d96752572fff4fc2e087d57c16ae36dd9
IronTaurus <irontauru@gmail.com>
2024-12-21 18:02:25 +0100

    test

--- labb3/app/page.tsx
+++ labb3/app/page.tsx
@@ -1,35 +1,24 @@
-'use client'
+"use client";
 import styles from "./page.module.css";
-import SideMenu from './components/SideMenu';
-import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
-import {Info, Training} from './pages/index'
+import SideMenu from "./components/SideMenu";
+import Link from 'next/link'
+import { Info, Training } from "./pages/index";
 
 export default function Home() {
-  const routes = [
-    {path: "info", element: <Info/>}, 
-    {path: "training", element: <Training/>}, 
-  ];
 
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className="App">
-          <header className="App-header">
-            <SideMenu/>
-            <div id="mid-col">
-              <Routes>
-                <Route path="/" element={<Outlet />}>
-                  <Route index element={<Home />} />
-                    {routes.map((route, i) => <Route key={i} path={route.path} element={route.element}/>)};
-                </Route>
-              </Routes>
-            </div>
-          </header>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <div className="App">
+            <header className="App-header">
+              <SideMenu />
+              <div id="mid-col">
+                <Link href={'./pages/Info'}/>
+              </div>
+            </header>
+          </div>
+        </main>
+        <footer className={styles.footer}></footer>
       </div>
-      </main>
-      <footer className={styles.footer}>
-
-      </footer>
-    </div>
   );
 }
